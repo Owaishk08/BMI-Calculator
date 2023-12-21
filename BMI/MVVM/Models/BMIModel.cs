17b5f9e5bdb66ffa93c42f3aac7c215231964b98
Mohd Owaish <114728256+Owaishk08@users.noreply.github.com>
2023-12-21 15:03:48 +0530

    Add files via upload

---
+++ BMI/MVVM/Models/BMIModel.cs
@@ -0,0 +1,65 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]   // Nuget Package Installed "PropertyChanged.Fody" 
+    public class BMIModel
+    {
+        private float result;
+
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public float Result 
+        { 
+            get 
+            {  
+                return ((Weight/Height)/Height*10000); 
+            } 
+        }
+
+        public string ResultText {  
+            get 
+            {
+                string Template = "BMI : #";
+                if (Result <= 16)
+                {
+                    return Template.Replace("#", "Severe Thinness");
+                }
+                else if (Result > 16 && Result <= 17)
+                {
+                    return Template.Replace("#", "Moderate Thinness");
+                }
+                else if(Result > 17 && Result <= 18.5)
+                {
+                    return Template.Replace("#", "Mild Thinness");
+                }
+                else if (Result > 18.5 && Result <= 25)
+                {
+                    return Template.Replace("#", "Normal");
+                }
+                else if (Result > 25 && Result <= 30)
+                {
+                    return Template.Replace("#", "OverWeight");
+                }
+                else if (Result > 30 && Result <= 35)
+                {
+                    return Template.Replace("#", "Obese Class I");
+                }
+                else if (Result > 35 && Result <= 40)
+                {
+                    return Template.Replace("#", "Obese Class II");
+                }
+                else
+                {
+                    return Template.Replace("#", "Obese Class III");
+                }
+            } 
+        }
+
+    }
+}
---
+++ BMI/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BMI.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI.MVVM.ViewModels
+{
+    
+    public class BMIViewModel
+    {   
+        public BMIModel BMIModel { get; set; }
+        public BMIViewModel() 
+        {
+            BMIModel = new BMIModel();
+            BMIModel.Height = 200;
+            BMIModel.Weight = 100;
+        }
+    }
+}
